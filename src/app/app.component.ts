c6888720c0cc5e429d165efd4bd233f4d05bfc1a
PapaStahl <christian@christianstahl.com>
2017-07-17 21:39:24 -0400

    Hero service with mock data and promise.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from './hero/hero';
+import { HeroService } from './hero/hero.service';
 
 
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
+
 
 @Component({
   selector: 'my-app',
   template: `<h1>{{title}}</h1>     
   <hero-list [heroes]="heroes"></hero-list>  
-  `
+  `,
+  providers: [ HeroService ]
 })
-export class AppComponent  {
-   title = 'Tour of Heroes'; 
-   
-   heroes = HEROES;
-   count = HEROES.length;   
+export class AppComponent implements OnInit  {
+   ngOnInit(): void{
+     this.getHeroes();
+   }
+   title = 'Tour of Heroes';    
+   heroes: Hero[];
+   count: number ;   
+
+   constructor( private heroService: HeroService){}
+
+   getHeroes():void{
+     this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+     this.count = this.heroes.length;
+   }
   }
